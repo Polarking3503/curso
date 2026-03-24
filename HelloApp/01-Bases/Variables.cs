d191a6346371c758a3c67abe6654c26a24551462
Polarking3503 <reynaga_45@hotmail.com>
2026-03-24 15:38:14 -0600

    Iniciar proyecto y tipos de variabes

---
+++ HelloApp/01-Bases/Variables.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+    static void Variables()
+    {
+        int edad = 25;
+        string nombre = "Jorge";
+        bool esEstudiante = false;
+
+        Console.WriteLine($"Nombre: {nombre} tiene una edad {edad} es estuduante {esEstudiante}");
+
+    }
+}
