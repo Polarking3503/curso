043b06f3a102833ff729e4793b93cf0369a173fd
Polarking3503 <reynaga_45@hotmail.com>
2026-04-09 17:46:33 -0600

    Actualizar secciones e indentacion

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,40 +1,42 @@
 ﻿partial class Program
 {
-  static void Main(string[] args)
-  {
-    #region Sección2
-    //Variables();
-    //DataTypes();
-    //ShowNumericTypes();
-    //ShowStringType();
-    //SalesReport();
-    //SalaryCalculator();
-    //ShowTime();
-    //DaysOfLife();
-    //DaysUntilNextBirthday();
-    // TypeDifference();
-    // HandleNullables();
-    // Operators();
-    // Arrays();
-    // InventoryManager();
-    // ListDictionary();
-    // DataStructures();
-    // Generics();
-    #endregion
-    #region Sección2
-    // Conditionals();
-    // Loops();
-    // FizzBuzz();
-    // LoopControlExamples();
-    // LoopGame();
-    // Conversions();
-    // ConvertionToDateTime();
-    // Functions();
-    // PrintMultiplicationTable(7, 15);
-    // PrintFactorialTable(5);
-    // Tuples();
-    // AnonymousFunctions();
-    TestingClass();
-    #endregion
-  }
+    static void Main(string[] args)
+    {
+        #region Sección2
+        //Variables();
+        //DataTypes();
+        //ShowNumericTypes();
+        //ShowStringType();
+        //SalesReport();
+        //SalaryCalculator();
+        //ShowTime();
+        //DaysOfLife();
+        //DaysUntilNextBirthday();
+        // TypeDifference();
+        // HandleNullables();
+        // Operators();
+        // Arrays();
+        // InventoryManager();
+        // ListDictionary();
+        // DataStructures();
+        // Generics();
+        #endregion
+        #region Sección3
+        // Conditionals();
+        // Loops();
+        // FizzBuzz();
+        // LoopControlExamples();
+        // LoopGame();
+        // Conversions();
+        // ConvertionToDateTime();
+        // Functions();
+        // PrintMultiplicationTable(7, 15);
+        // PrintFactorialTable(5);
+        // Tuples();
+        // AnonymousFunctions();
+        #endregion
+        #region Sección4
+        TestingClass();
+        #endregion
+    }
 }
