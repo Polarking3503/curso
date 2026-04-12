04cf2a357b8a7dd944dcdff0eaa8b31205d79836
Polarking3503 <reynaga_45@hotmail.com>
2026-04-12 17:07:03 -0600

    Creación e instanciación de objetos

--- HelloApp/03-clases/Methods.cs
+++ HelloApp/03-clases/Methods.cs
@@ -6,18 +6,54 @@ partial class Program
     car.Model = "Yaris";
     car.Year = 2022;
     WriteLine(car.ShowInfo());
-    car.ShowMessage();
-    car.ShowMessage("Cambiando de modelo");
-    car.ChangeModel("Patrol");
-    WriteLine(car.ShowInfo());
+    // car.ShowMessage();
+    // car.ShowMessage("Cambiando de modelo");
+    // car.ChangeModel("Patrol");
+    // WriteLine(car.ShowInfo());
 
-    Car.GeneralInfo();
+    // Car.GeneralInfo();
+
+    // Un constructor
+    Car sportsCar = new("Ferrari", 2020);
+    WriteLine(sportsCar.ShowInfo());
+
+    //Sintaxis simplificada
+    Car collectionCar = new Car { Model = "Cadillac", Year = 1980 };
+    WriteLine(collectionCar.ShowInfo());
+
+    // Lista de objetos
+    WriteLine("Listado de autómoviles: ");
+    List<Car> cars = new()
+    {
+      new Car(){Model="Duster",Year=2021},
+      new Car(){Model="StepWay",Year=2019},
+      new Car(){Model="Captur",Year=200},
+    };
+    foreach (var item in cars)
+    {
+      WriteLine(item.ShowInfo());
+    }
   }
 }
 class Car
 {
   public string? Model { get; set; }
   public int? Year { get; set; }
+
+  //Constructor con parametros
+  public Car(string model, int year)
+  {
+    Model = model;
+    Year = year;
+  }
+  //Constructor por defecto
+  public Car() { }
+
+  //Destructor (~)
+  ~Car()
+  {
+    WriteLine("Destructor llamado. Recurso liberado");
+  }
 
   public void ChangeModel(string newModel)
   {
