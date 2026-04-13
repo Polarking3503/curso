aaa7ab2fe9703cc3bee9665fb95cd4158eda5cd0
Polarking3503 <reynaga_45@hotmail.com>
2026-04-12 18:35:07 -0600

    Visibilidad (public, private, protected) e interfaces

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -46,7 +46,8 @@
         // ProductSalesDemo();
         //  BusFleet();
         // Inheritance();
-        AbstracClassExamples();
+        // AbstracClassExamples();
+        Visibility();
         #endregion
     }
 }
