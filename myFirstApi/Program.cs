f3e7358f7f18f2d6bc7ad4828b8e6dc0d8da2707
Polarking3503 <reynaga_45@hotmail.com>
2026-04-17 17:16:07 -0600

    Organización del código con archivos estáticos

--- myFirstApi/Program.cs
+++ myFirstApi/Program.cs
@@ -13,6 +13,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/index.html");
+    }
+    else
+    {
+        await next();
+    }
+
+});
 
 var summaries = new[]
 {
@@ -21,7 +34,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
