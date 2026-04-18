51d38db852d66eb52e7e658ad183dd8ce353a82a
Polarking3503 <reynaga_45@hotmail.com>
2026-04-17 23:18:38 -0600

    TaskMasterAPI

---
+++ TaskMasterAPI/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
